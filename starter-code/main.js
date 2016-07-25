ac9f14bd863292eaee461cdb64c40d674bc23eb4
Caz Brunnen <cazbrunnen@cazs-mbp.lan>
2016-07-25 18:00:59 +0100

    commiting unit 8.7

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+/* Don't think i need to link this here..
+<script type="text/html" src="/Users/cazbrunnen/Desktop/coding/index3.html"></script> */
+
+
+
